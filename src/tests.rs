562eb4c58c88d1734159bca724ef4518eb63dd3b
jaime gonzalez fabregas <jaimegonzalezfabregas@gmail.com>
2023-06-11 15:46:34 +0200

    done

--- src/tests.rs
+++ src/tests.rs
@@ -400,9 +400,9 @@ mod tests {
     fn conectivity_problem() {
          let mut engine = Engine::new();
         assert_eq!(
-            "\n(1, 1)\n(1, 2)\n(1, 3)\n(1, 4)\n(2, 1)\n(2, 2)\n(2, 3)\n(2, 4)\n(3, 1)\n(3, 2)\n(3, 3)\n(3, 4)\n(4, 1)\n(4, 2)\n(4, 3)\n(4, 4)",
+            "\n(1, 1)\n(1, 2)\n(1, 3)\n(1, 4)\n(2, 1)\n(2, 2)\n(2, 3)\n(2, 4)\n(3, 1)\n(3, 2)\n(3, 3)\n(3, 4)\n(4, 1)\n(4, 2)\n(4, 3)\n(4, 4)\n",
             engine.input(
-                "edge(1,2) edge(3,4) edge(2,3) conected(a,b) :- conected(a,mid) && edge(mid,b) conected(a,a):- true conected(b,a) :- conected(a,b) conected(_,_)?".into(),
+                "edge(1,2) edge(3,4) edge(2,3) conected(a,b) :- conected(a,mid) && edge(mid,b) conected(a,a):- true edge(b,a) :- edge(a,b) conected(_,_)?".into(),
                 false
             )
         );
